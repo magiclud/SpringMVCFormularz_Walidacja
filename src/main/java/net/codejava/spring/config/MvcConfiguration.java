c80b7f27a672f93047f6b70254a74bcae1903b01
magiclud <agnieszkalud@op.pl>
2014-09-17 19:20:28 +0200

    user uklepszony do hibernate

--- src/main/java/net/codejava/spring/config/MvcConfiguration.java
+++ src/main/java/net/codejava/spring/config/MvcConfiguration.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@SuppressWarnings("unused")
 @Configuration
 @ComponentScan(basePackages = "net.codejava.spring")
 //@PropertySource("classpath:/message_en.properties")
--- src/main/java/net/codejava/spring/model/User.java
+++ src/main/java/net/codejava/spring/model/User.java
@@ -6,6 +6,12 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import net.codejava.spring.validator.Phone;
 
 import org.hibernate.validator.constraints.Email;
@@ -14,25 +20,46 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.format.annotation.DateTimeFormat;
 
+@Entity
+// tells Hibernate that this class represents an object that we can persist.
+@Table(name = "USERS")
+// annotation tells Hibernate which table to map properties in this class to
 public class User {
-//	@Size(min = 2, max = 30)
+
+	@Id
+	// will be unique for all events persisted
+	@Column(name = "ID")
+	@GeneratedValue
+	// annotation says that this value will be determined by the datasource, not
+	// by the code.
+	private Integer id;
+
+	// @Size(min = 2, max = 30)
 	@NotEmpty
+	@Column(name = "USERNAME")
+	// annotation is used to map this property to the USERNAME column in the
+	// USERS table.
 	private String username;
 	@NotNull
 	@Size(min = 5)
+	@Column(name = "PASSWORD")
 	private String password;
 	@NotEmpty
 	@Email
+	@Column(name = "EMAIL")
 	private String email;
 	@DateTimeFormat(pattern = "MM/dd/yyyy")
 	@NotNull
 	@Past
+	@Column(name = "BIRTHDATE")
 	private Date birthDate;
 	private String profession;
 	@Phone
-	@Size(min=2, max=9)
+	@Size(min = 2, max = 9)
+	@Column(name = "PHONE")
 	private String phone;
 	@NotNull
+	@Column(name = "GENDER")
 	private Gender gender;
 
 	public enum Gender {
@@ -94,6 +121,14 @@ public class User {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	// getters and setters...
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 }
